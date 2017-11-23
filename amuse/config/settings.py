b02ee071c15b157d9743b8918a8a526226c200b8
sergio <sergio@sergio.torrado>
2017-11-22 19:49:18 -0500

    Config DB

--- amuse/config/settings.py
+++ amuse/config/settings.py
@@ -86,13 +86,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DEFAULT_DATABASES = 'sqlite:///%s' % BASE_DIR.child('db.sqlite3')
 
+DATABASES = {
+    'default': dj_database_url.config(default=DEFAULT_DATABASES)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
